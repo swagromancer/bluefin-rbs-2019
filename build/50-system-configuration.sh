29dc2ae3fe743d46bedbfe58c311ae4af99c7555
Cory Graham <coryg2178@gmail.com>
2026-04-16 18:09:21 -0400

    feat: add developer packages and configs from bluefin-dx

--- build/50-system-configuration.sh
+++ build/50-system-configuration.sh
@@ -12,9 +12,13 @@ set -eoux pipefail
 # TODO: Finish configuring services
 
 # Enable/disable systemd services
+systemctl --global enable podman-auto-update.timer
 systemctl enable podman.socket
 systemctl enable flatpak-preinstall.service
 systemctl enable ublue-nvidia-flatpak-runtime-sync.service
+systemctl enable dev-groups.service
+systemctl enable swtpm-workaround.service
+systemctl enable libvirt-workaround.service
 
 # Enable Homebrew services copied from @ublue-os/brew
 systemctl preset brew-setup.service
